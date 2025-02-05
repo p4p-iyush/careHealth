659db1cff6765cf5bceb24a4944c9551e8080eb2
Piyush Jain <jain4piyush@gmail.com>
2025-02-05 13:59:02 +0530

    updates till now

--- Frontend/src/Components/UserModules/Pharmacists/Prescription/PrescriptionDetail.jsx
+++ Frontend/src/Components/UserModules/Pharmacists/Prescription/PrescriptionDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./PrescriptionDetails.css";
+import "./PrescriptionDetail.css";
 
 const PrescriptionDetails = () => {
   const [prescriptions, setPrescriptions] = useState([]);
